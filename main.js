a24921611463dba04508d400d331d2380e891d35
donny_444 <104888835+donny444@users.noreply.github.com>
2023-02-20 23:20:21 +0700

    website worked. (without form validation :,()

--- main.js
+++ main.js
@@ -1,31 +1,86 @@
-const weight = document.getElementById("weight")
-const height = document.getElementById("height")
-const age = document.getElementById("age")
-const sex = document.getElementsByClassName("sex")
-const frequently = document.getElementsByClassName("frequently")
-document.getElementById("submit").addEventListener("click",calculate())
-function calculate() {
-    var bmi = (weight/((height*height)/10000))
-    if (sex = "Male")
-        var bmr = 66+(13.7*weight)+(5*height)-(6.8*age)
-    if (sex = "Female")
-        var bmr = 665+(9.6*weight)+(1.8*height)-(4.7*age)
-    for (i=0;i<=4;i++)
-        var c = [1.2, 1.375, 1.55, 1.7, 1.9]
-        if (frequently = document.querySelectorAll("frequently")[i])
-            var tdee = c[i]*bmr
+document.getElementById("submit").addEventListener("click", e => {
+    e.preventDefault()
+    main()
+})
+/*function main() {
+    define()
+    validationAll()
+    calculate()
     display()
 }
-function display() {
-    document.querySelector("#bmi").innerHTML = bmi
-    document.querySelector("#bmr").innerHTML = bmr
-    document.querySelector("#tdee").innerHTML = tdee
+function define() {
+    const weight = parseInt(document.getElementById("weight").value)
+    const height = parseInt(document.getElementById("height").value)
+    const age = parseInt(document.getElementById("age").value)
+    const sex = document.getElementById("sex").value
+    const frequently = parseInt(document.getElementById("frequently").value)
 }
 function validation(param) {
-    if (param.validity.typemisMatch) {
-        param.setCustomValidity("TYPE THE PROPER!!!")
+    if (param.validity.valueMissing) {
+        param.setCustomValidity("ENTER THE VALUE!!!")
+    }
+    else {
+        param.setCustomValidity("")
     }
 }
-validation(weight)
-validation(height)
-validation(age)
+function validationAll() {
+    validation(weight)
+    validation(height)
+    validation(age)
+}
+function calculate() {
+    var bmi = ((weight/((height*height)/10000))).toString()
+    if (sex = "Male")
+        var bmr = (66+(13.7*weight)+(5*height)-(6.8*age)).toString()
+    if (sex = "Female")
+        var bmr = (665+(9.6*weight)+(1.8*height)-(4.7*age)).toString()
+    var tdee = (frequently*bmr).toString()
+}
+function display() {
+    document.getElementById("bmi").innerHTML = bmi
+    document.getElementById("bmr").innerHTML = bmr
+    document.getElementById("tdee").innerHTML = tdee
+}*/
+function main() {
+    const weight = parseInt(document.getElementById("weight").value)
+    const height = parseInt(document.getElementById("height").value)
+    const age = parseInt(document.getElementById("age").value)
+    const sex = document.getElementById("sex").value
+    const frequently = document.getElementById("frequently").value
+    /*if (weight.validity.valueMissing) {
+        weight.setCustomValidity("ENTER THE VALUE!!!")
+    }
+    else {
+        weight.setCustomValidity("")
+    }
+    if (height.validity.valueMissing) {
+        height.setCustomValidity("ENTER THE VALUE!!!")
+    }
+    else {
+        height.setCustomValidity("")
+    }
+    if (age.validity.valueMissing) {
+        age.setCustomValidity("ENTER THE VALUE!!!")
+    }
+    else {
+        age.setCustomValidity("")
+    }*/
+    //validationAll()
+    var bmi = ((weight/((height*height)/10000)))
+    if (sex === "Male")
+        var bmr = (66+(13.7*weight)+(5*height)-(6.8*age))
+    if (sex === "Female")
+        var bmr = (665+(9.6*weight)+(1.8*height)-(4.7*age))
+    var tdee = (frequently*bmr)
+    document.getElementById("bmi").innerHTML = bmi.toString()
+    document.getElementById("bmr").innerHTML = bmr.toString()
+    document.getElementById("tdee").innerHTML = tdee.toString()
+}
+function validation(param)
+    while (param.validity.valueMissing) {
+        param.setCustomValidity("ENTER THE VALUE!!!")
+    }
+function validationAll()
+    validation(weight)
+    validation(height)
+    validation(age)
