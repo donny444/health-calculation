700c45674f2d0495782f5a3c907f5851adf9d8d0
Donny <tanadornrach@gmail.com>
2024-02-27 23:12:58 +0700

    unfinished

---
+++ v2.0/health_calculation.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2024 at 05:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `health_calculation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `values`
+--
+
+CREATE TABLE `values` (
+  `id` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `frequently` decimal(4,3) NOT NULL,
+  `bmi` int(11) NOT NULL,
+  `bmr` int(11) NOT NULL,
+  `tdee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `values`
+--
+ALTER TABLE `values`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `values`
+--
+ALTER TABLE `values`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- v2.0/submit.php
+++ v2.0/submit.php
@@ -12,10 +12,17 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $weight = $_POST["weight"];
-    $height = $_POST["height"];
-    $age = $_POST["age"];
+    $weight = (int)$_POST["weight"];
+    $height = (int)$_POST["height"];
+    $age = (int)$_POST["age"];
     $sex = $_POST["sex"];
-    $frequently = $_POST["frequently"];
+    $frequently = (float)$_POST["frequently"];
 
-    $stmt = $conn->prepare("INSERT INTO health_calculation")
+    $bmi = (int)($weight/($height*$height)/10000)
+    if($sex === "Male")
+    {
+        $bmr = (int)(66+(13.7*$weight))
+    }
+
+    $stmt = $conn->prepare("INSERT INTO health_calculation (weight, height, age, sex, frequently, bmi, bmr, tdee) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("iiisiiii");
